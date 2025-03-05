ac2a1f9bb4992ff2e313b6a08ea7155bcf48c349
tjddn <tjddn3927@naver.com>
2025-03-05 16:37:18 +0900

    init

---
+++ src/main/java/com/study/aws_docker_jenkins_study/AwsDockerJenkinsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.aws_docker_jenkins_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDockerJenkinsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDockerJenkinsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/aws_docker_jenkins_study/AwsDockerJenkinsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.aws_docker_jenkins_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDockerJenkinsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
